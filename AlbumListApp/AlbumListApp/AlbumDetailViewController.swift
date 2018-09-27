86912909f5922a960058febdf56753c596ac4c04
Reşat Pekgözlü <reshat52@hotmail.com>
2018-09-27 09:30:47 +0200

    getDataApiService

--- AlbumListApp/AlbumListApp/AlbumDetailViewController.swift
+++ AlbumListApp/AlbumListApp/AlbumDetailViewController.swift
@@ -8,23 +8,96 @@
 
 import UIKit
 
-class AlbumDetailViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupThumbnailImage(url: String){
+        thumbnailImageView.loadImageUsingUrlString(urlString: url)
     }
-    */
+}
 
+class AlbumDetailViewController: UIViewController {
+
+    var photos = [Photo]()
+    @IBOutlet weak var photosCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let width = (view.frame.size.width - 10) / 2
+        let layout = photosCollectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.itemSize = CGSize(width: width, height: width * 1.5)
+        
+    }
+    
+    func updateCollectionContent(userId: Int) {
+        
+        let service = ApiService()
+        service.getAlbumWithUserId(userId: userId) { (result) in
+            switch result {
+            case .Success(let data):
+                self.photos = data
+                DispatchQueue.main.async {
+                    self.photosCollectionView.reloadData()
+                }
+            case .Error(let message):
+                DispatchQueue.main.async {
+                    self.showAlertWith(title: "Error", message: message)
+                }
+            }
+        }
+    }
+    
+    func showAlertWith(title: String, message: String, style: UIAlertController.Style = .alert) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: style)
+        let action = UIAlertAction(title: "OK", style: .default) { (action) in
+            self.dismiss(animated: true, completion: nil)
+        }
+        alertController.addAction(action)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "showPhoto"){
+            let cell = sender as! PhotosCollectionViewCell
+            let destination = segue.destination as! PhotoViewController
+            destination.title = cell.titleLabel.text
+            destination.imageUrl = photos[(photosCollectionView.indexPath(for: cell)?.row)!].url
+        }
+    }
+    
+}
+extension AlbumDetailViewController: UICollectionViewDelegate,UICollectionViewDataSource{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = photosCollectionView.dequeueReusableCell(withReuseIdentifier: "photosCollectionCell", for: indexPath) as! PhotosCollectionViewCell
+        cell.titleLabel.text = photos[indexPath.row].title
+        cell.setupThumbnailImage(url: photos[indexPath.row].thumbnailUrl)
+        
+        return cell
+    }
+
+}
+
+extension UIImageView {
+    
+    func loadImageUsingUrlString(urlString: String) {
+        guard let url = URL(string: urlString) else { return }
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            DispatchQueue.main.async {
+                self.image = UIImage(data: data!)
+            }
+            
+            }.resume()
+    }
 }
--- AlbumListApp/AlbumListApp/AlbumsViewController.swift
+++ AlbumListApp/AlbumListApp/AlbumsViewController.swift
@@ -8,23 +8,72 @@
 
 import UIKit
 
+class AlbumCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+}
+
 class AlbumsViewController: UIViewController {
 
+    var albums = [Album]()
+    @IBOutlet weak var albumsCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let width = (view.frame.size.width - 10) / 2
+        let layout = albumsCollectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.itemSize = CGSize(width: width, height: width * 1.3)
+        updateCollectionContent()
+    }
+    func updateCollectionContent() {
+        
+        let service = ApiService()
+        service.getDataWith { (result) in
+            switch result {
+            case .Success(let data):
+                self.albums = data
+                DispatchQueue.main.async {
+                    self.albumsCollectionView.reloadData()
+                }
+            case .Error(let message):
+                DispatchQueue.main.async {
+                    self.showAlertWith(title: "Error", message: message)
+                }
+            }
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func showAlertWith(title: String, message: String, style: UIAlertController.Style = .alert) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: style)
+        let action = UIAlertAction(title: "OK", style: .default) { (action) in
+            self.dismiss(animated: true, completion: nil)
+        }
+        alertController.addAction(action)
+        self.present(alertController, animated: true, completion: nil)
     }
-    */
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "showPhotos"){
+            let cell = sender as! AlbumCollectionViewCell
+            let destination = segue.destination as! AlbumDetailViewController
+            
+            destination.title = cell.titleLabel.text
+            let userId = albums[(albumsCollectionView.indexPath(for: cell)?.row)!].id
+            destination.updateCollectionContent(userId: userId)
+        }
+    }
 
+}
+
+extension AlbumsViewController:UICollectionViewDelegate,UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return albums.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = albumsCollectionView.dequeueReusableCell(withReuseIdentifier: "albumCollectionCell", for: indexPath) as! AlbumCollectionViewCell
+        cell.titleLabel.text = albums[indexPath.row].title
+        
+        return cell
+    }
 }
