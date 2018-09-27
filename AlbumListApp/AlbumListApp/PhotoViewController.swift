57d4963d5a47048e6e546dc672d63902f3c395e1
Reşat Pekgözlü <reshat52@hotmail.com>
2018-09-27 09:13:27 +0200

    addViewControllers

---
+++ AlbumListApp/AlbumListApp/PhotoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoViewController.swift
+//  AlbumListApp
+//
+//  Created by Resat Pekgozlu on 9/27/18.
+//  Copyright © 2018 Resat Pekgozlu. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
